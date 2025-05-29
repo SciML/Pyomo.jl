50bc1ca4eb9e0d59b379ea870cf487f438d53c80
vyudu <vincent.duyuan@gmail.com>
2025-05-29 10:27:41 -0400

    add julia compat

--- src/Pyomo.jl
+++ src/Pyomo.jl
@@ -23,6 +23,7 @@ const dae = PythonCall.pynew()
 const opt = PythonCall.pynew()
 const math = PythonCall.pynew()
 const compare_expressions = PythonCall.pynew()
+
 function __init__() 
     PythonCall.pycopy!(pyomo, pyimport("pyomo.environ"))
     PythonCall.pycopy!(dae, pyimport("pyomo.dae"))
